72645fd8482e5196e82d8f071738d77618f31c3a
tom_chen <tom_chen@mail.chungyo.net>
2022-02-26 15:06:56 +0800

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,56 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:react/recommended',
+    'react-app',
+    'react-app/jest',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 6,
+    sourceType: 'module',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx'],
+      },
+    },
+    react: {
+      version: 'detect',
+    },
+  },
+  plugins: [
+    'react',
+    'jsx-a11y',
+    'import',
+  ],
+  ignorePatterns: ['config', 'public', 'scripts'],
+  rules: {
+    'class-methods-use-this': ['error', {
+      exceptMethods: [
+        'render',
+        'getInitialState',
+        'getDefaultProps',
+      ],
+    }],
+    'global-require': 0,
+    'no-control-regex': 0,
+    'import/no-named-as-default': 0,
+    'max-len': ['error', { code: 120 }],
+    'react/display-name': 0,
+    'no-plusplus': 0,
+  },
+};
